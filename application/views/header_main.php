63467761ec4bd5e3bbb3d8c2cff46785ef7eb452
WereDouglas <weredouglas@gmail.com>
2016-02-17 10:27:29 +0300

    added header things
    added header things

--- application/views/header_main.php
+++ application/views/header_main.php
@@ -51,63 +51,15 @@
                             </ul>
                         </div>
                     </div>
-                    <a href="javascript:void(0);" class="add_user" data-toggle="modal" data-target="#myModal"> <i class="fa fa-plus-square"></i> <span> New Task</span> </a>
-                    <div class="top_right_bar">
+                     <div class="top_right_bar">
                         <div class="top_right">
                             <div class="top_right_menu">
-                                <ul>
-                                    <li class="dropdown"> <a href="javascript:void(0);" data-toggle="dropdown"> Tasks <span class="badge badge">8</span> </a>
-                                        <ul class="drop_down_task dropdown-menu">
+                                <ul>                                 
+                                   
+                                    <li class="dropdown"> 
                                             <div class="top_pointer"></div>
-                                            <li>
-                                                <p class="number">You have 7 pending tasks</p>
-                                            </li>
-                                            <li> <a href="task.html" class="task">
-                                                    <div class="green_status task_height" style="width:80%;"></div>
-                                                    <div class="task_head"> <span class="pull-left">Task Heading</span> <span class="pull-right green_label">80%</span> </div>
-                                                    <div class="task_detail">Task details goes here</div>
-                                                </a> </li>
-                                            <li> <a href="task.html" class="task">
-                                                    <div class="yellow_status task_height" style="width:50%;"></div>
-                                                    <div class="task_head"> <span class="pull-left">Task Heading</span> <span class="pull-right yellow_label">50%</span> </div>
-                                                    <div class="task_detail">Task details goes here</div>
-                                                </a> </li>
-                                            <li> <a href="task.html" class="task">
-                                                    <div class="blue_status task_height" style="width:70%;"></div>
-                                                    <div class="task_head"> <span class="pull-left">Task Heading</span> <span class="pull-right blue_label">70%</span> </div>
-                                                    <div class="task_detail">Task details goes here</div>
-                                                </a> </li>
-                                            <li> <a href="task.html" class="task">
-                                                    <div class="red_status task_height" style="width:85%;"></div>
-                                                    <div class="task_head"> <span class="pull-left">Task Heading</span> <span class="pull-right red_label">85%</span> </div>
-                                                    <div class="task_detail">Task details goes here</div>
-                                                </a> </li>
-                                            <li> <span class="new"> <a href="task.html" class="pull_left">Create New</a> <a href="task.html" class="pull-right">View All</a> </span> </li>
-                                        </ul>
-                                    </li>
-                                    <li class="dropdown"> <a href="javascript:void(0);" data-toggle="dropdown"> Mail <span class="badge badge color_1">4</span> </a>
-                                        <ul class="drop_down_task dropdown-menu">
-                                            <div class="top_pointer"></div>
-                                            <li>
-                                                <p class="number">You have 4 mails</p>
-                                            </li>
-                                            <li> <a href="readmail.html" class="mail"> <span class="photo"><img src="images/user.png" /></span> <span class="subject"> <span class="from">sarat m</span> <span class="time">just now</span> </span> <span class="message">Hello,this is an example msg.</span> </a> </li>
-                                            <li> <a href="readmail.html" class="mail"> <span class="photo"><img src="images/user.png" /></span> <span class="subject"> <span class="from">sarat m</span> <span class="time">just now</span> </span> <span class="message">Hello,this is an example msg.</span> </a> </li>
-                                            <li> <a href="readmail.html" class="mail red_color"> <span class="photo"><img src="images/user.png" /></span> <span class="subject"> <span class="from">sarat m</span> <span class="time">just now</span> </span> <span class="message">Hello,this is an example msg.</span> </a> </li>
-                                            <li> <a href="readmail.html" class="mail"> <span class="photo"><img src="images/user.png" /></span> <span class="subject"> <span class="from">sarat m</span> <span class="time">just now</span> </span> <span class="message">Hello,this is an example msg.</span> </a> </li>
-
-                                        </ul>
-                                    </li>
-                                    <li class="dropdown"> <a href="javascript:void(0);" data-toggle="dropdown"> notification <span class="badge badge color_2">6</span> </a>
-                                        <div class="notification_drop_down dropdown-menu">
-                                            <div class="top_pointer"></div>
-                                            <div class="box"> <a href="inbox.html"> <span class="block primery_6"> <i class="fa fa-envelope-o"></i> </span> <span class="block_text">Mailbox</span> </a> </div>
-                                            <div class="box"> <a href="calendar.html"> <span class="block primery_2"> <i class="fa fa-calendar-o"></i> </span> <span class="block_text">Calendar</span> </a> </div>
-                                            <div class="box"> <a href="maps.html"> <span class="block primery_4"> <i class="fa fa-map-marker"></i> </span> <span class="block_text">Map</span> </a> </div>
-                                            <div class="box"> <a href="todo.html"> <span class="block primery_3"> <i class="fa fa-plane"></i> </span> <span class="block_text">To-Do</span> </a> </div>
-                                            <div class="box"> <a href="task.html"> <span class="block primery_5"> <i class="fa fa-picture-o"></i> </span> <span class="block_text">Tasks</span> </a> </div>
-                                            <div class="box"> <a href="timeline.html"> <span class="block primery_1"> <i class="fa fa-clock-o"></i> </span> <span class="block_text">Timeline</span> </a> </div>
-                                        </div>
+                                                <div class="box"> <a target="myframe" href="<?php echo base_url() . "index.php/Welcome/schedule"; ?>"> <span class="block primery_2"> <i class="fa fa-calendar-o"></i> </span> <span class="block_text">Calendar</span> </a> </div>
+                                               
                                     </li>
                                 </ul>
                             </div>
@@ -130,12 +82,7 @@
             </div>
             <!--\\\\\\\ header end \\\\\\-->
             <div class="inner">
-                <!--\\\\\\\ inner start \\\\\\--><div class="left_nav">
-
-                    <!--\\\\\\\left_nav start \\\\\\-->
-                    <div class="search_bar"> <i class="fa fa-search"></i>
-                        <input name="" type="text" class="search" placeholder="Search Dashboard..." />
-                    </div>
+                
                     <div class="left_nav_slidebar">
                         <ul>
                             <li class="left_nav_active theme_border"><a href="javascript:void(0);"><i class="fa fa-home"></i> DASHBOARD <span class="left_nav_pointer"></span> <span class="plus"><i class="fa fa-plus"></i></span> </a>
@@ -183,41 +130,22 @@
                             </li>
                             <li> <a href="javascript:void(0);"> <i class="fa fa-th"></i> SYNOPTICS <span class="plus"><i class="fa fa-plus"></i></span> </a>
                                 <ul>
-                                    <li>
-                                        <a target="myframe" href="<?php echo base_url() . "index.php/synoptic/"; ?>">Tabular</a>
-                                    </li>
-                                    <li>
-                                        <a target="myframe" href="<?php echo base_url() . "index.php/synoptic/tab"; ?>">Tabbed</a>
-                                    </li>  </ul>
+                                    <li> <a target="myframe" href="<?php echo base_url() . "index.php/synoptic/tab"; ?>"> <span>&nbsp;</span> <i class="fa fa-circle"></i> <b>Tabbed</b> </a> </li>
+                             <li> <a target="myframe" href="<?php echo base_url() . "index.php/synoptic/"; ?>"> <span>&nbsp;</span> <i class="fa fa-circle"></i> <b>Table</b> </a> </li>
+                            
+                                      </ul>
                             </li>
                             <li> <a href="javascript:void(0);"> <i class="fa fa-cog"></i> SETTINGS <span class="plus"><i class="fa fa-plus"></i></span></a>
                                 <ul>
-                                    <li>
-                                        <a target="myframe" href="<?php echo base_url() . "index.php/station"; ?>">Stations</a>
-                                    </li>
-                                    <li>
-                                        <a target="myframe" href="<?php echo base_url() . "index.php/user/"; ?>">User</a>
-                                    </li>
-
-                                    <li>
-                                        <a target="myframe" href="<?php echo base_url() . "index.php/role/"; ?>">Roles</a>
-                                    </li>
-                                    <li>
-                                        <a target="myframe" href="<?php echo base_url() . "index.php/logs/"; ?>">Logs</a>
-                                    </li>
-                                    <li>
-                                        <a target="myframe" href="<?php echo base_url() . "index.php/element/"; ?>">Elements </a>
-                                    </li>
-                                    <li>
-                                    </li>
-                                    <li>
-                                        <a target="myframe" href="<?php echo base_url() . "index.php/instrument"; ?>">  <i class="icon-cogs">Instrument </i></a>
-                                    </li>
-                                    <li>
-                                        <a target="myframe" href="<?php echo base_url() . "index.php/archive"; ?>"><i class="icon-cogs">Archive</i>
-                                        </a>
-                                    </li>
-
+                                      <li> <a target="myframe" href="<?php echo base_url() . "index.php/station/"; ?>"> <span>&nbsp;</span> <i class="fa fa-circle"></i> <b>Stations</b> </a> </li>
+                                  
+                                     <li> <a target="myframe" href="<?php echo base_url() . "index.php/user/"; ?>"> <span>&nbsp;</span> <i class="fa fa-circle"></i> <b>Users</b> </a> </li>
+                                    <li> <a target="myframe" href="<?php echo base_url() . "index.php/role/"; ?>"> <span>&nbsp;</span> <i class="fa fa-circle"></i> <b>Roles</b> </a> </li>
+                                    <li> <a target="myframe" href="<?php echo base_url() . "index.php/logs/"; ?>"> <span>&nbsp;</span> <i class="fa fa-circle"></i> <b>Logs</b> </a> </li>
+                                    <li> <a target="myframe" href="<?php echo base_url() . "index.php/element/"; ?>"> <span>&nbsp;</span> <i class="fa fa-circle"></i> <b>Elements</b> </a> </li>
+                                    <li> <a target="myframe" href="<?php echo base_url() . "index.php/instrument/"; ?>"> <span>&nbsp;</span> <i class="fa fa-circle"></i> <b>Instruments</b> </a> </li>
+                                    <li> <a target="myframe" href="<?php echo base_url() . "index.php/archive/"; ?>"> <span>&nbsp;</span> <i class="fa fa-circle"></i> <b>Archive</b> </a> </li>
+                                 
                                 </ul>
                             </li>
                         </ul>
